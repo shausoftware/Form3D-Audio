73a2351303fa785c223ebcd0ef49786195cabbfd
scotthare <the.sh.in.it@gmail.com>
2019-08-31 18:23:38 +0100

    Fixed background drift from camera

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -3,10 +3,19 @@
 public class Background : MonoBehaviour {
 
 	private Material material;
+	private Camera mainCamera;
 
 	void Start () {
 		material = transform.GetComponent<Renderer>().material; //background material
+		mainCamera = Camera.main;
 		Keyboard.changeBackground += UpdateShaderBackground;
+	}
+
+	void Update() {
+		//opposite camera
+		Vector3 direction = -mainCamera.transform.position.normalized;
+		transform.position = direction * 20; 
+		transform.rotation = mainCamera.transform.rotation;
 	}
 	
 	void UpdateShaderBackground(int backgroundId) {
--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -7,12 +7,10 @@ public class CameraMovement : MonoBehaviour {
     private TimerUtils timerUtils;
 	private Vector3 targetDirection = Vector3.zero;
 	private AudioPlayer audioPlayer;
-	private GameObject background;
 
 	void Start() {
 		timerUtils  = new TimerUtils(BPMUtils.GetDuration(BPM, 8));
 		audioPlayer = FindObjectOfType<AudioPlayer>();
-		background = GameObject.Find("Background");
 	}
 
 	void Update () {
@@ -26,12 +24,11 @@ public class CameraMovement : MonoBehaviour {
 		float rms = audioPlayer.GetRms();
 		float at32 = Time.time % BPMUtils.GetDuration(BPM, 32);
 		float wobble = Mathf.Sin(at32*28) * rms*0.6f;
-        wobble += Mathf.Sin(at32*28) * 0.8f * //frequency and amplitude 
-		          Mathf.Max(1 - at32*0.6f, 0); //decay
+        wobble += Mathf.Sin(at32*28)*0.8f * //frequency and amplitude 
+		          Mathf.Max(1 - at32*0.5f, 0); //decay
 
 		transform.Translate(bounceDirection*wobble);
 		transform.RotateAround(Vector3.zero, targetDirection, 20 * Time.deltaTime);
-		background.transform.RotateAround(Vector3.zero, targetDirection, 20 * Time.deltaTime);
 		transform.LookAt(Vector3.zero);
 	}
 
